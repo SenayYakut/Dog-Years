8e9396d632d46735d5fe4952234ba79551249a43
Senay Yakut <senaykt@gmail.com>
2021-05-31 09:00:43 -0700

    Add the code

---
+++ app.js
@@ -0,0 +1,16 @@
+// my age
+let myAge = 41;
+//my dogs age
+let earlyYears = 2;
+earlyYears *= 10.5;
+//The first two years has different values
+laterYears = myAge - 2;
+//After the first two years dog years values
+laterYears *= 4;
+//My age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+//my name in lower case
+let myName = "Aiden".toLowerCase();
+let myNameAndDogAge = `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+console.log(myNameAndDogAge);
